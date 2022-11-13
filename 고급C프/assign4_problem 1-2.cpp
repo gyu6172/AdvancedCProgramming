b1d8fd10f81a61ff8c4a2a31bf55a7ca4cc3c14f
gyu6172 <gyu6172@gmail.com>
2022-11-13 12:30:41 +0900

    과제4 - 동적할당 완료

--- 고급C프/assign4_problem 1-2.cpp
+++ 고급C프/assign4_problem 1-2.cpp
@@ -1,54 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//구조체 선언
 typedef struct st {
-	char *str;
-	int cnt1;
-	int cnt2;
-	int num;
-	int pos;
-	int result;
+	char *str;		//문자열 저장
+	int cnt1;		//자음 수 저장
+	int cnt2;		//총 문자 수 저장
+	int num;		//숫자 저장
+	int pos;		//위치 저장
+	int result;		//곱셈결과 저장
 }St;
 int main() {
-	int n,i,j;
-	scanf("%d",&n);
-	St* stArr=(St*)malloc(sizeof(St)*n);
-	if (stArr == NULL) {
-		printf("Not Enough Memory");
-		return -1;
+	int n,i,j;		//반복문 순회용 변수 선언
+	scanf("%d",&n);	//n 입력받음
+	St* stArr=(St*)malloc(sizeof(St)*n);	//n만큼의 크기를 가진 구조체 배열 동적할당
+	if (stArr == NULL) {	//만약 할당이 정상적으로 되지 않았다면
+		printf("Not Enough Memory");	
+		return -1;			//프로그램 종료
 	}
+	//n번 반복하며 문자열 입력받음
 	for (i = 0; i < n; i++) {
-		char str[101];
-		getchar();
-		scanf("%s",str);
-		int len=strlen(str);
-		stArr[i].str = (char*)malloc(sizeof(char)*(len+1));
-		if (stArr[i].str == NULL) {
+		char str[101];	//입력받을 문자를 저장해놓을 배열 선언
+		getchar();		//개행문자 제거
+		scanf("%s",str);	
+		int len=strlen(str);	//문자열 길이 저장
+		stArr[i].str = (char*)malloc(sizeof(char)*(len+1));		//문자열의 길이+1 만큼의 메모리 공간 동적할당
+		if (stArr[i].str == NULL) {		//할당이 정상적으로 되지 않았다면
 			printf("Not Enough Memory");
-			return -1;
+			return -1;					//프로그램 종료
 		}
-		strcpy(stArr[i].str, str);
+		strcpy(stArr[i].str, str);		//할당 받은 공간에 입력받은 문자열 복사
 	}
+	//n번 돌면서 구조체의 나머지 값들을 초기화한다.
 	for (i = 0; i < n; i++) {
-		int moumCnt=0;
-		for (j = 0; j < strlen(stArr[i].str); j++) {
+		int moumCnt=0;		//모음의 개수를 저장할 변수 선언
+		for (j = 0; j < strlen(stArr[i].str); j++) {	//문자열의 처음부터 끝까지 확인하면서
+			//만약 모음이 나온다면
 			if (stArr[i].str[j] == 'a' || stArr[i].str[j] == 'e' || stArr[i].str[j] == 'i' || stArr[i].str[j] == 'o' || stArr[i].str[j] == 'u'
 				|| stArr[i].str[j] == 'A' || stArr[i].str[j] == 'E' || stArr[i].str[j] == 'I' || stArr[i].str[j] == 'O' || stArr[i].str[j] == 'U') {
-				moumCnt++;
+				moumCnt++;	//모음의 개수 1 증가
 			}
+			//숫자가 나온다면
 			else if ('0' <= stArr[i].str[j] && stArr[i].str[j] <= '9') {
-				stArr[i].num = stArr[i].str[j] - '0';
-				stArr[i].pos = j+1;
+				stArr[i].num = stArr[i].str[j] - '0';	//구조체의 num값에 숫자 저장
+				stArr[i].pos = j+1;						//그 숫자의 위치 저장
 			}
 		}
+		//자음 개수 = 문자열 길이 - 모음 개수 - 1(숫자)
 		stArr[i].cnt1 = strlen(stArr[i].str) - moumCnt - 1;
-		stArr[i].cnt2 = strlen(stArr[i].str);
-		stArr[i].result = (stArr[i].cnt1)*(stArr[i].cnt2)*(stArr[i].num)*(stArr[i].pos);
+		stArr[i].cnt2 = strlen(stArr[i].str);	//cnt2에 문자열의 길이 저장
+		stArr[i].result = (stArr[i].cnt1)*(stArr[i].cnt2)*(stArr[i].num)*(stArr[i].pos);	//모두 곱해서 result에 결과값 저장
 	}
 
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n - i - 1; j++) {
-			if (stArr[j].result < stArr[j + 1].result) {
+	//result가 큰 순서대로 내림차순 정렬
+	for (i = 0; i < n; i++) {	//i는 0~n까지
+		for (j = 0; j < n - i - 1; j++) {	//j는 0부터 n-i-1 까지 돌면서
+			if (stArr[j].result < stArr[j + 1].result) {	//만약 j의 result < j+1의 result 라면
+				//서로 스왑(버블정렬)
 				St tmp = stArr[j];
 				stArr[j] = stArr[j+1];
 				stArr[j+1] = tmp;
@@ -56,12 +64,15 @@ int main() {
 		}
 	}
 
+	//정렬된 순서대로 문자열 출력
 	for (i = 0; i < n; i++) {
 		printf("%s\n",stArr[i].str);
 	}
 
+	//문자열 메모리 할당 해제
 	for (i = 0; i < n; i++) {
 		free(stArr[i].str);
 	}
+	//구조체 배열 할당 해젠
 	free(stArr);
 }
--- 고급C프/assign4_problem 2-3.cpp
+++ 고급C프/assign4_problem 2-3.cpp
@@ -2,49 +2,71 @@
 #include <string.h>
 #include <stdlib.h>
 int main() {
+	//A, B 문자열 선언
 	char A[101], B[101];
+	//A와 B 입력
 	gets_s(A);
 	gets_s(B);
 
+	//A의 공백 개수, B의 공백 개수를 저장할 변수
 	int AblankCnt = 0, BblankCnt = 0;
-	int i, j, k;
+	int i, j, k;		//반복문 순회용 변수
 	for (i = 0; i < strlen(A); i++) {
+		//A를 돌면서 공백이 있으면
 		if (A[i] == ' ') {
+			//공백 개수 1증가
 			AblankCnt++;
 		}
 	}
+	//B를 돌면서 공백이 있으면
 	for (i = 0; i < strlen(B); i++) {
+		//공백개수 1 증가
 		if (B[i] == ' ') {
 			BblankCnt++;
 		}
 	}
 
+	//A의 단어들과 B의 단어들을 저장할 문자열 배열 선언하고 동적할당 받기
 	char** Awords = (char**)malloc(sizeof(char*) * (AblankCnt + 1));
 	char** Bwords = (char**)malloc(sizeof(char*) * (BblankCnt + 1));
 
+	//만약 동적할당이 제대로 되지 않았다면
 	if (Awords == NULL || Bwords == NULL) {
 		printf("Not Enough Memory!");
+		//프로그램 종료
 		return -1;
 	}
 
+	//A를 돌며 현재 단어를 저장할 배열 선언
 	char tmpStr[101];
+	//tmpStr의 길이
 	int tmpIter = 0;
+	//A의 단어의 개수
 	int AwordsIter = 0;
 	for (i = 0; i < strlen(A); i++) {
+		//A를 돌면서 공백이 나온다면
 		if (A[i] == ' ') {
+			//tmpStr 마지막에 널문자 추가함.
 			tmpStr[tmpIter++] = NULL;
+			//tmpStr의 길이만큼 동적할당 받음
 			Awords[AwordsIter] = (char*)malloc(sizeof(char) * tmpIter);
+			//동적할당이 제대로 되지 않았다면
 			if (Awords[AwordsIter] == NULL) {
 				printf("Not Enough Memory!");
+				//프로그램 종료
 				return -1;
 			}
+			//동적할당 받은 공간에 단어 저장
 			strcpy(Awords[AwordsIter++], tmpStr);
+			//tmpStr의 길이 초기화
 			tmpIter = 0;
 		}
 		else {
+			//그 외의 경우에는 단어에 A의 문자를 추가함
 			tmpStr[tmpIter++] = A[i];
 		}
 	}
+	//A 순회가 끝나면 똑같은 과정을 한 번 더 진행함.
 	tmpStr[tmpIter++] = NULL;
 	Awords[AwordsIter] = (char*)malloc(sizeof(char) * tmpIter);
 	if (Awords[AwordsIter] == NULL) {
@@ -54,22 +76,31 @@ int main() {
 	strcpy(Awords[AwordsIter++], tmpStr);
 	tmpIter = 0;
 
+	//A와 같은 방법으로 B도 진행함
 	int BwordsIter = 0;
+	//B 문자열 순회함
 	for (i = 0; i < strlen(B); i++) {
+		//만약 공백이 나온다면
 		if (B[i] == ' ') {
+			//단어 끝에 널문자 추가
 			tmpStr[tmpIter++] = NULL;
+			//단어의 길이만큼 동적할당 받는다.
 			Bwords[BwordsIter] = (char*)malloc(sizeof(char) * tmpIter);
+			//동적할당이 제대로 되지 않았다면
 			if (Bwords[BwordsIter] == NULL) {
 				printf("Not Enough Memory!");
 				return -1;
 			}
+			//동적할당 받은 공간에 문자열 복사하여 저장
 			strcpy(Bwords[BwordsIter++], tmpStr);
 			tmpIter = 0;
 		}
 		else {
+			//그 외의 경우에는 문자 추가
 			tmpStr[tmpIter++] = B[i];
 		}
 	}
+	//B 순회가 끝나면 단어를 한 번 더 추가해 준다.
 	tmpStr[tmpIter++] = NULL;
 	Bwords[BwordsIter] = (char*)malloc(sizeof(char) * tmpIter);
 	if (Bwords[BwordsIter] == NULL) {
@@ -79,30 +110,48 @@ int main() {
 	strcpy(Bwords[BwordsIter++], tmpStr);
 	tmpIter = 0;
 
-	//printf("Aword : %d, Bword : %d\n",AwordsIter, BwordsIter);
-	//for (i = 0; i < AwordsIter; i++) {
-	//	printf("%s\n",Awords[i]);
-	//}
-	//for (i = 0; i < BwordsIter; i++) {
-	//	printf("%s\n", Bwords[i]);
-	//}
-
-	char resultStr[101][101];
+	//최종적으로 출력할 단어들이 저장된 문자열의 배열
+	//배열의 길이가 저장된 변수
 	int resultIter = 0;
-
+	//문자열 배열 동적 할당
+	char** resultStr = (char**)malloc(sizeof(char*));
 	for (i = 0; i < AwordsIter; i++) {
 		for (j = i + 1; j < AwordsIter; j++) {
 			//AcombStr1은 Awords[i] 뒤에 Awords[j]를 붙인 것이고,
 			//AcombStr2는 Awords[j] 뒤에 Awords[i]를 붙인 것이다.
 			char AcombStr1[201], AcombStr2[201];
+			//먼저 AcombStr1에 Awords[[i]를 복사하고,
 			strcpy(AcombStr1, Awords[i]);
+			//그 뒤에 Awords[j]를 붙임.
 			strcat(AcombStr1, Awords[j]);
 
+			//같은 방법으로 AcombStr2에는 Awords[j]를 복사하고, 그 뒤에 Awords[i]를 붙임.
 			strcpy(AcombStr2, Awords[j]);
 			strcat(AcombStr2, Awords[i]);
 
+			//B의 단어들이 저장된 배열을 쭉 순회함.
 			for (k = 0; k < BwordsIter; k++) {
+				//만약 AcombStr1이나 AcombStr2와 똑같은 단어가 있다면
 				if (strcmp(AcombStr1, Bwords[k]) == 0 || strcmp(AcombStr2, Bwords[k]) == 0) {
+					//그 단어를 resultStr에 넣어야함.
+					//문자열 배열의 크기를 모르기 때문에 realloc으로 다시 할당해준다.
+					//resultIter+1의 크기만큼 다시 할당해준다.
+					resultStr = (char**)realloc(resultStr, sizeof(char*)*(resultIter+1));
+					//동적할당이 제대로 되지 않았을 경우
+					if (resultStr == NULL) {
+						printf("Not Enough Memory");
+						//프로그램 종료
+						return -1;
+					}
+					//문자배열 메모리 동적할당(Bwords[k]의 길이+1 만큼)
+					resultStr[resultIter] = (char*)malloc(sizeof(char)*(strlen(Bwords[k])+1));
+					//동적할당이 제대로 되지 않았을 경우
+					if (resultStr[resultIter] == NULL) {
+						printf("Not Enough Memory");
+						//프로그램 종료
+						return -1;
+					}
+					//동적할당 한 공간에 문자열을 넣어준다.
 					strcpy(resultStr[resultIter++], Bwords[k]);
 				}
 			}
@@ -110,8 +159,11 @@ int main() {
 		}
 	}
 
+
+	//문자열의 길이가 긴 순서대로 정렬하는 코드
 	for (i = 0; i < resultIter; i++) {
 		for (j = i; j < resultIter; j++) {
+			//j보다 j+1이 길면 서로 스왑(버블 정렬)
 			if (strlen(resultStr[i]) < strlen(resultStr[j])) {
 				char tmpStr1[101];
 				strcpy(tmpStr1, resultStr[i]);
@@ -121,10 +173,14 @@ int main() {
 		}
 	}
 
+	//문자열의 길이가 같을 때는 사전 순서대로 정렬해야함.
 	for (i = 0; i < resultIter; i++) {
 		for (j = i; j < resultIter; j++) {
+			//문자열의 길이가 같고
 			if (strlen(resultStr[i]) == strlen(resultStr[j])) {
+				//j보다 j+1이 사전 순으로 빠르다면
 				if (strcmp(resultStr[i], resultStr[j]) > 0) {
+					//서로 스왑(버블정렬)
 					char tmpStr1[101];
 					strcpy(tmpStr1, resultStr[i]);
 					strcpy(resultStr[i], resultStr[j]);
@@ -134,14 +190,34 @@ int main() {
 		}
 	}
 
+	//만약 똑같은 값이 있어도 한 번만 출력해야 함.
 	for (i = 0; i < resultIter; i++) {
+		//i가 0일 때는 그냥 출력하고
 		if (i == 0) {
 			printf("%s\n", resultStr[i]);
 		}
+		//그 외의 경우에는 이전 인덱스의 문자열하고 다를 때만 출력
 		else if (strcmp(resultStr[i], resultStr[i - 1])!=0) {
 			printf("%s\n", resultStr[i]);
 		}
 	}
+
+	//메모리 할당 해제
+	//resultStr 메모리 할당 해제
+	for (i = 0; i < resultIter; i++) {
+		free(resultStr[i]);
+	}
+	free(resultStr);
+	//Awords 메모리 할당 해제
+	for (i = 0; i < AwordsIter; i++) {
+		free(Awords[i]);
+	}
+	free(Awords);
+	//Bwords 메모리 할당 해제
+	for (i = 0; i < BwordsIter; i++) {
+		free(Bwords[i]);
+	}
+	free(Bwords);
 
 
 }
